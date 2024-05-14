5a5276fb5cf0eb756dbcf7f497e6d9974be001f0
Shadrak Lagadi <shadrl19@gmail.com>
2024-05-14 13:12:51 -0600

    Finsih main.rb

---
+++ main.rb
@@ -0,0 +1,9 @@
+require_relative 'game'
+
+puts "Enter name for Player 1:"
+player1_name = gets.chomp
+puts "Enter name for Player 2:"
+player2_name = gets.chomp
+
+game = Game.new(player1_name, player2_name)
+game.play_game
